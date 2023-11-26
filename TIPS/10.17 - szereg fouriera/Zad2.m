7996785eeec097768e8a4a22eb724bbf5261193a
JackobPunch <jakubcios99@gmail.com>
2023-11-26 14:42:51 +0100

    first batch of fourier series

--- TIPS/10.17 - szereg fouriera/Zad2.m
+++ TIPS/10.17 - szereg fouriera/Zad2.m
@@ -3,7 +3,7 @@ clear
 close all
 
 T=4;
-N=50;
+N=13;
 nt=1000;
 t=linspace(0,T,nt);
 X0=0.5;
@@ -15,7 +15,7 @@ E=T/2-T*X0.^2;
 for n=1:N
     Xn=((-1).^n-1)/(-j*2*pi*n);
     A(n+1)=2*j*Xn;
-    E(n+1)=E(n)-T*2*abs(Xn)^2
+    E(n+1)=E(n)-T*2*abs(Xn)^2;
     x=x+Xn*(exp(j*2*pi*n*t/T)-exp(-j*2*pi*n*t/T));
     plot(t,x)
     pause(0.1)
