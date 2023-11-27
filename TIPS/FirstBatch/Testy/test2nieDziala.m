3592bfdb4e6329870b62cd4052d7ee49e49f3104
JackobPunch <jakubcios99@gmail.com>
2023-11-27 21:48:51 +0100

    tiding

--- TIPS/FirstBatch/Testy/test2nieDziala.m
+++ TIPS/FirstBatch/Testy/test2nieDziala.m
@@ -1,3 +1,4 @@
+%tu chyba podejmowałem jakąś próbę zrobienia w pętli
 clear all
 close all
 N=20;
@@ -10,9 +11,9 @@ x1=2*(cos(t)+j*sin(t));
 x2=4j*(cos(2*t)+j*sin(2*t));
 x3=6*(cos(3*t)+j*sin(3*t));
 x4=8j*(cos(4*t)+j*sin(4*t));
-
+x=[x1 x2 x3 x4];
 for n=1:N
-    x=x0+xn*(exp(j*w0*n*t)+exp(-j*w0*n*t));
+    x(n+1)=x(n)*(exp(j*w0*n*t)+exp(-j*w0*n*t));
 end
 var()
     
